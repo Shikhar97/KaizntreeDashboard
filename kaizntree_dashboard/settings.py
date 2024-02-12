4de5a3f7186606bd57515c2a0e0ab0c0f681dd0b
Shikhar Gupta <sgupt330@asu.edu>
2024-02-12 15:00:03 -0700

    Update settings.py

--- kaizntree_dashboard/settings.py
+++ kaizntree_dashboard/settings.py
@@ -171,7 +171,7 @@ REST_FRAMEWORK = {
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379",
+        "LOCATION": "redis://127.0.0.1:6380",
     }
 }
 
