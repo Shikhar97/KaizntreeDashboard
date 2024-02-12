5aeae529af4525e3c7cd54362f929075ebb53150
Shikhar Gupta <sgupt330@asu.edu>
2024-02-12 14:59:21 -0700

    Update settings.py

--- kaizntree_dashboard/settings.py
+++ kaizntree_dashboard/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config.get('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
