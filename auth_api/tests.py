876d3475061166e063c0fa15736171df29ccc518
Shikhar Gupta <sgupt330@asu.edu>
2024-02-11 20:26:59 -0700

    Added Unit test for auth_api

--- auth_api/tests.py
+++ auth_api/tests.py
@@ -1,3 +1,77 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from auth_api.models import User
+
+
+class AuthApiTests(APITestCase):
+    def test_create_account(self):
+        """
+        Register a new user.
+        """
+        url = reverse('register')
+        data = {
+            'email': 'testcase1@gmail.com',
+            'password1': 'pass$12345',
+            'password2': 'pass$12345'
+        }
+        response = self.client.post(url, data, format='json')
+        response_data = response.json()
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertTrue('username' in response_data)
+        self.assertEqual(response_data['username'], data['email'])
+
+        # Ensure password is not returned
+        self.assertFalse('password' in response_data)
+
+    def test_login_account(self):
+        """
+        Login with a user.
+        """
+        user = User.objects.create_user(username='testcase2@gmail.com', password='pass$12345')
+        url = reverse('login')
+        data = {
+            'username': 'testcase2@gmail.com',
+            'password': 'pass$12345',
+        }
+        response = self.client.post(url, data, format='json')
+        response_data = response.json()
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        self.assertTrue('username' in response_data)
+        self.assertEqual(response_data['username'], data['email'])
+
+        self.assertTrue('access_token' in response_data)
+        self.assertTrue('refresh_token' in response_data)
+
+        # Ensure password is not returned
+        self.assertFalse('password' in response_data)
+
+    def test_login_invalid_username(self):
+        """
+        Login fails with invalid username.
+        """
+        url = reverse('login')
+        data = {
+            'username': 'testcase3@gmail.com',
+            'password': 'pass$12345',
+        }
+        response = self.client.post(url, data, format='json')
+        response_data = response.json()
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_login_fail(self):
+        """
+        Login fails with wrong password.
+        """
+        url = reverse('login')
+        data = {
+            'username': 'testcase4@gmail.com',
+            'password': 'pass$12345',
+        }
+        response = self.client.post(url, data, format='json')
+        response_data = response.json()
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
--- auth_api/urls.py
+++ auth_api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import RegisterUser, LoginUser, ResetPassword
 
 urlpatterns = [
-    path('login/', LoginUser.as_view()),
-    path('register/', RegisterUser.as_view()),
-    path('reset_password/', ResetPassword.as_view()),
+    path('login/', LoginUser.as_view(), name='login'),
+    path('register/', RegisterUser.as_view(), name='register'),
+    path('reset_password/', ResetPassword.as_view(), name='reset_password'),
 ]
